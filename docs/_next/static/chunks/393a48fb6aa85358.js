816a41e7e4f795ffcbe8702f833b04273b2dcdbe
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-08 11:11:41 +0700

    update next.js

--- docs/_next/static/chunks/393a48fb6aa85358.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/6f090b0dec478d76.js",
-  "static/chunks/47717ffd58795a1e.js",
-  "static/chunks/98794ff496ae7050.js",
-  "static/chunks/turbopack-ee6d7f067b4a0024.js"
-])
--- docs/_next/static/chunks/7ec3eb23630418b4.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/20769591110a23fa.js",
-  "static/chunks/98794ff496ae7050.js",
-  "static/chunks/adc2da416e546d77.css",
-  "static/chunks/turbopack-23805a6bf87d7429.js"
-])
