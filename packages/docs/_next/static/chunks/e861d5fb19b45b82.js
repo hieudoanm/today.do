86d2b25b795fc176a00a0539ac0789d17072907a
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-11-08 11:09:38 +0700

    update folder

---
+++ packages/docs/_next/static/chunks/e861d5fb19b45b82.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/a87045a98e0fe277.js",
+  "static/chunks/98794ff496ae7050.js",
+  "static/chunks/adc2da416e546d77.css",
+  "static/chunks/turbopack-2fa639d58aede45a.js"
+])
---
+++ packages/docs/_next/static/kW6eLA4I4ybHS4qbk0nKX/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/cddd40670c8890da.js"
+  ],
+  "/_error": [
+    "static/chunks/0f6d64d761fd8c1c.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
